072aca070055daa2d17a55b80bfa421ab1e86571
keremkabiloglu <keremkabiloglu@kolayfirsat.com>
2024-02-21 21:28:36 +0300

    Update deploy.sh script to transfer files to multipass node

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@
 
 # Define variables
 DOCKER_IMAGE_NAME="user-service"
-K8S_DEPLOYMENT_FILE="user-service.yaml"
 
 # Remove existing Docker image
 echo "Removing existing Docker image"
@@ -20,21 +19,18 @@ docker save -o $DOCKER_IMAGE_NAME.tar $DOCKER_IMAGE_NAME
 
 # Remove existing image from k8s
 echo "Removing existing Docker image from k8s"
-microk8s ctr images remove docker.io/library/$DOCKER_IMAGE_NAME:latest
+multipass exec microk8s-vm -- microk8s ctr images remove docker.io/library/$DOCKER_IMAGE_NAME:latest
 
-# Transfer tar file to k8s node
-echo "Transferring Docker image to k8s"
-multipass transfer $DOCKER_IMAGE_NAME.tar microk8s-vm:/tmp/$DOCKER_IMAGE_NAME.tar
+# Transfer files to multipass node
+echo "Transferring files to multipass node"
+multipass exec microk8s-vm -- mkdir -p /tmp/$DOCKER_IMAGE_NAME
+multipass transfer $DOCKER_IMAGE_NAME.tar microk8s-vm:/tmp/$DOCKER_IMAGE_NAME/$DOCKER_IMAGE_NAME.tar
+multipass transfer $DOCKER_IMAGE_NAME.yaml microk8s-vm:/tmp/$DOCKER_IMAGE_NAME/$DOCKER_IMAGE_NAME.yaml
+multipass transfer .env-deployment microk8s-vm:/tmp/$DOCKER_IMAGE_NAME/.env-deployment
 
 # Load tar file to k8s
 echo "Loading Docker image to k8s"
-microk8s ctr images import /tmp/$DOCKER_IMAGE_NAME.tar
-
-
-
-# Remove tar file from k8s node
-echo "Removing tar file from k8s node"
-multipass exec microk8s-vm -- rm /tmp/$DOCKER_IMAGE_NAME.tar
+multipass exec microk8s-vm -- microk8s ctr images import /tmp/$DOCKER_IMAGE_NAME/$DOCKER_IMAGE_NAME.tar
 
 # Remove tar file from local
 echo "Removing tar file from local"
@@ -42,20 +38,24 @@ rm $DOCKER_IMAGE_NAME.tar
 
 # Create Kubernetes secret
 echo "Creating Kubernetes secret"
-microk8s kubectl delete secret $DOCKER_IMAGE_NAME-secret --ignore-not-found
-microk8s kubectl create secret generic $DOCKER_IMAGE_NAME-secret --from-env-file=.env-deployment
+multipass exec microk8s-vm -- microk8s kubectl delete secret $DOCKER_IMAGE_NAME-secret --ignore-not-found
+multipass exec microk8s-vm -- microk8s kubectl create secret generic $DOCKER_IMAGE_NAME-secret --from-env-file=/tmp/$DOCKER_IMAGE_NAME/.env-deployment
 
 # Remove existing Kubernetes deployment and service
 echo "Removing existing Kubernetes deployment and service"
-microk8s kubectl delete deployment $DOCKER_IMAGE_NAME --ignore-not-found
+multipass exec microk8s-vm -- microk8s kubectl delete deployment $DOCKER_IMAGE_NAME --ignore-not-found
 
 # Apply Kubernetes deployment and service configurations
 echo "Applying Kubernetes deployment and service configurations"
-microk8s kubectl apply -f $K8S_DEPLOYMENT_FILE
+multipass exec microk8s-vm -- microk8s kubectl apply -f /tmp/$DOCKER_IMAGE_NAME/$DOCKER_IMAGE_NAME.yaml
 
 
 # Show Kubernetes pods status
 echo "Pods status:"
-microk8s kubectl get pods
+multipass exec microk8s-vm -- microk8s kubectl get pods
+
+# Delete tmp folder
+echo "Deleting tmp folder"
+#multipass exec microk8s-vm -- rm -rf /tmp/$DOCKER_IMAGE_NAME
 
 echo "Deployment completed."
